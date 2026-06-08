0bf7d1096cd2eb32d8ec53f5c032a2ace790b52b
Eesha Patel <eesha.patel07@gmail.com>
2026-06-08 18:16:34 -0400

    Add login and register functionality to frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,53 @@
+import { useState } from "react";
 
 
 export default function Login() {
-    return (
-        <>
+    const [error, setError] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-        </>
+    const handleLogin = async () => {
+        try {
+            const response = await fetch("https://localhost:5000/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                },
+                body: {
+                    username,
+                    password
+                }
+            });
+
+            if (response.ok) {
+                // Navigate to dashboard
+            }
+        } catch (err) {
+            console.log(err);
+            setError("Login failed");
+        }
+    }
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={username}
+                onChange={(text) => setUsername(text)}
+                placeholder="Username"
+            />
+
+            <input
+                type="text"
+                value={password}
+                onChange={(text) => setPassword(text)}
+                placeholder="Password"
+            />
+
+            <button onClick={handleLogin} >
+                Login
+            </button>
+        </div>
     )
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,52 @@
 
 
 export default function Register() {
+    const [error, setError] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const handleRegister = async () => {
+        try {
+            const response = await fetch("https://localhost:5000/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: {
+                    username,
+                    password
+                }
+            });
+            if (response.ok) {
+                // Navigate to login page
+            }
+
+
+        } catch (err) {
+            console.log(err);
+            setError("Failed to register");
+        }
+    }
     return (
-        <>
-        </>
+        <div>
+            <input
+                type="text"
+                value={username}
+                onChange={(text) => setUsername(text)}
+                placeholder="Username"
+            />
+
+            <input
+                type="text"
+                value={password}
+                onChange={(text) => setPassword(text)}
+                placeholder="Password"
+            />
+
+            <button onClick={handleRegister}>
+                Register
+            </button>
+        </div>
     )
 }
